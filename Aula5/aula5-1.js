d7dd943f4d74b8f28fc9f0fb2124224e44cb91ff
Leonardo Castro <142045008+LeoCasttr0@users.noreply.github.com>
2024-11-08 11:21:34 -0300

    aula sobre tipos primitivos de js, focado em string, number e boolean

---
+++ Aula5/aula5-1.js
@@ -0,0 +1,42 @@
+// Tipos de dados primitivos do JavaScript
+// String : cadeia de caractéres ( letras,caracteres,números )
+// Number : números
+//Boolean : true ou false
+// null : nulo
+// undefined :
+
+// ----------------------------------------------------------
+// String
+// "Ola";
+// 'Ola';
+// `Ola`;
+// ("123"); ele não é um tipo number pois ele esta usando aspas;
+
+//não ha muita regra ao usar as aspas simples ou duplas. ha nao ser que voce queira que as aspas façam parte do seu conteudo , como :
+// console.log('"citação"');
+
+// podemos concatenar strings
+// console.log("ola " + "mundo");
+
+// ----------------------------------------------------------
+// Number
+// 2;
+// 2.5;
+// 0.0;
+// 0.2;
+
+// console.log(2 + 2);
+// console.log(6 - 2);
+// console.log(2 * 5);
+// console.log(20 / 2);
+// console.log(0 / 0);
+// console.log(1 / 0);
+
+// ----------------------------------------------------------
+//Boolean : nome dado pelo seu criador
+// true
+// false
+// usado dependendo de algumas situaçoes futuras
+
+// console.log(!true)
+// console.log(!false);
